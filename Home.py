cd546ab90732d9697c35c47eff20252cdfc67f1b
Reinaldo Silva <reinaldoblack@hotmail.com>
2023-11-02 14:52:12 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -66,7 +66,8 @@ if select_menu == "Página Inicial":
     col1, col2 = st.columns(2, gap='small')
 
     # Define a largura da coluna da imagem
-    st.image(profile_pic, width=450)
+    with col1:
+        st.image(profile_pic, width=450)
 
     with col2:
         st.title(name)
